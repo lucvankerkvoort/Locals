3a2e8c14deae1c3924a4b12d250244e72600cdb2
Tangerinez <gtang159@gmail.com>
2019-07-29 00:05:54 -0700

    Created Accordion and Chevron symbol for Infocards with React Hooks and dangerouslySetHTML

---
+++ views/src/components/General/InfoCards/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Accordion from "./Accordion";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Accordion
+        title="Section 1?"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      />
+      <Accordion
+        title="Section 2"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      />
+      <Accordion
+        title="Section 3"
+        content="
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        "
+      />
+    </div>
+  );
+}
+
+export default App;
