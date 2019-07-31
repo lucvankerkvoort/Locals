1f3252547ed908097824626c6479a2fc6f970963
Chiefcos <41163851+Chiefcos@users.noreply.github.com>
2019-07-31 15:57:15 -0700

    made the Loginpage responsive for locals and travellers

--- views/src/components/LoginPage/images/index.js
+++ views/src/components/LoginPage/images/index.js
@@ -5,6 +5,11 @@ import Image4 from "./traveller4.jpg";
 import Image5 from "./traveller5.jpg";
 import Image6 from "./traveller6.jpg";
 
-const travellerArray = [Image1, Image2, Image3, Image4, Image5, Image6];
+import Picture1 from "./Local1.jpg";
+import Picture2 from "./Local2.jpg";
+import Picture3 from "./Local3.jpg";
+import Picture4 from "./Local4.jpg";
+import Picture5 from "./Local5.jpg";
 
-export default travellerArray;
+export const travellerArray = [Image1, Image2, Image3, Image4, Image5, Image6];
+export const localArray = [Picture1, Picture2, Picture3, Picture4, Picture5];
