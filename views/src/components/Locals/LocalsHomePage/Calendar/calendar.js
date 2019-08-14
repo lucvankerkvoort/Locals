9fac75446ec9051fd47e223ce797a7610fd4b9dd
Chiefcos <41163851+Chiefcos@users.noreply.github.com>
2019-08-13 21:42:52 -0700

    created the calendar page

--- views/src/components/Locals/LocalsHomePage/Calendar/calendar.js
+++ views/src/components/Locals/LocalsHomePage/Calendar/calendar.js
@@ -1,13 +1,34 @@
 import React from "react";
+import DateRangePicker from "@wojtekmaj/react-daterange-picker";
+import "./style.css";
 
-class Calendar extends React.Component {
+class Availability extends React.Component {
+  state = {
+    date: [new Date(), new Date()]
+  };
+  handleChange = date => {
+    this.setState({ date });
+  };
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
+      <div className="calendar-body">
+        <div className="calendar">
+          <DateRangePicker
+            onChange={this.handleChange}
+            value={this.state.date}
+          />
+          <button className="btn calendar-button">Submit</button>
+        </div>
+
+        <div className="available-title">
+          <h3>Availability</h3>
+        </div>
+        <div className="availability">
+          <div className="available-dates" />
+        </div>
       </div>
     );
   }
 }
 
-export default Calendar;
+export default Availability;
