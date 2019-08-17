546de28e96f1dc8d11c644caaa34b214829e5ce8
Chiefcos <41163851+Chiefcos@users.noreply.github.com>
2019-08-17 09:40:55 -0700

    fixed the problem with the route for the dates

--- views/src/controller/index.js
+++ views/src/controller/index.js
@@ -2,35 +2,35 @@ import axios from "axios";
 
 export default {
   getLocals: function() {
-    return axios.get("api/locals");
+    return axios.get("/api/locals");
   },
   getTravelers: function() {
-    return axios.get("api/traveller");
+    return axios.get("/api/traveller");
   },
   pushLocals: function(dataLocal) {
     console.log(dataLocal);
-    return axios.post("api/locals", dataLocal);
+    return axios.post("/api/locals", dataLocal);
   },
   pushTravelers: function(dataTraveler) {
     console.log(dataTraveler);
-    return axios.post("api/traveller", dataTraveler);
+    return axios.post("/api/traveller", dataTraveler);
   },
   loginSearchLocal: function(data) {
     console.log(data);
-    return axios.post("api/locals/login/", data);
+    return axios.post("/api/locals/login/", data);
   },
   loginSearchTraveler: function(data) {
-    return axios.post("api/traveller/login/", data);
+    return axios.post("/api/traveller/login/", data);
   },
   getLocalById: function(id) {
-    return axios.get("api/locals/" + id);
+    return axios.get("/api/locals/" + id);
   },
   getTravelerById: function(id) {
-    return axios.get("api/traveller/" + id);
+    return axios.get("/api/traveller/" + id);
   },
   pushDates: function(id, data) {
     console.log("ID", id, "Data", data);
-    return axios.post("api/locals/dates/" + id, { date: data });
+    return axios.post("/api/locals/dates/" + id, { date: data });
   },
   updateLocal: function(id, data) {
     return axios.post("/api/locals/" + id, data);
