4bdf914f30783e4124b28fa11ad959582ae53dbe
Tangerinez <gtang159@gmail.com>
2019-08-17 11:22:59 -0700

    Added date filter to traveler's home page

---
+++ views/src/components/Travelers/TravelerSearchPage/DateContainer/StartDateInput.js
@@ -0,0 +1,37 @@
+import React from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "./StartDateInput.css";
+
+class StartDateInput extends React.Component {
+  state = {
+    startDate: new Date()
+  };
+
+  handleChange = date => {
+    this.setState({
+      startDate: date
+    });
+  };
+
+  render() {
+    console.log(this.state.startDate);
+    return (
+      <div className="start-date-container">
+        <div className="start-date-search">
+          <form>
+            <DatePicker
+              className="start-date-searchTerm"
+              selected={this.state.startDate}
+              onChange={this.handleChange}
+              name="startDate"
+              dateFormat="mm/dd/yyyy"
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default StartDateInput;
