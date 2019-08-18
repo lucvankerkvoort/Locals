52292f229e9c51f572e9e5ececae147590d51ae4
Chiefcos <41163851+Chiefcos@users.noreply.github.com>
2019-08-17 19:10:29 -0700

    setup the back-end route to find people by place and tested it

--- views/src/components/Travelers/TravelerHome/DateContainer/DateContainer.js
+++ views/src/components/Travelers/TravelerHome/DateContainer/DateContainer.js
@@ -3,13 +3,34 @@ import "./DateContainer.css";
 import StartDateInput from "./StartDateInput";
 import EndDateInput from "./EndDateInput";
 
-const DateContainer = () => {
-  return (
-    <div className="dates-container">
-      <StartDateInput />
-      <EndDateInput />
-    </div>
-  );
-};
+let dates = [];
+class DateContainer extends React.Component {
+  handleStartDate = input => {
+    console.log(input);
+    dates.push(input);
+    this.handleDates();
+  };
+  handleEndDate = input => {
+    console.log(input);
+    dates.push(input);
+    this.handleDates();
+  };
+
+  handleDates = () => {
+    console.log(dates);
+    if (dates.length <= 2) {
+      this.props.handleDates(dates);
+    }
+  };
+
+  render() {
+    return (
+      <div className="dates-container">
+        <StartDateInput startDate={this.handleStartDate} />
+        <EndDateInput endDate={this.handleEndDate} />
+      </div>
+    );
+  }
+}
 
 export default DateContainer;
