c3598d0d0cc3439bf203e807872f4ea531b9cb89
Chiefcos <41163851+Chiefcos@users.noreply.github.com>
2019-08-23 19:09:29 -0700

    forgot to pull yours, did that now too

--- controller/localController.js
+++ controller/localController.js
@@ -82,12 +82,21 @@ module.exports = {
     );
     // here we will use mongo's queries to find a specific document and delete it from the database
   },
+  deleteBooking: function(req, res) {
+    console.log("Delete Field Value", req.params.id, req.body);
+    db.Local.update(
+      { _id: req.params.id },
+      { $pull: { booking: { id: { $in: [req.body.id] } } } }
+    )
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.json(err));
+  },
   deleteFieldValue: function(req, res) {
     console.log("Delete Field Value", req.params.id, req.body);
     db.Local.update(
       { _id: req.params.id },
       {
-        $pullAll: { availability: req.body }
+        $pull: { availability: req.body }
       }
     )
       .then(dbModel => res.json(dbModel))
