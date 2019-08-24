ca1cde6000ca1ad81784aee17e9541bec6074897
Chiefcos <41163851+Chiefcos@users.noreply.github.com>
2019-08-24 09:05:55 -0700

    started to work on the tours page, not all the way done

--- views/src/components/Travelers/TravelerToursPage/ToursBody/ToursBody.js
+++ views/src/components/Travelers/TravelerToursPage/ToursBody/ToursBody.js
@@ -1,37 +1,62 @@
 import React from "react";
 import "./ToursBody.css";
 import Accordion from "../Accordion/Accordion";
+import Moment from "react-moment";
+import API from "../../../../controller";
 
 class ToursBody extends React.Component {
   state = {
-    booking: ""
+    booking: "",
+    check: false
   };
 
   componentDidMount() {
     this.handleUser();
   }
 
+  setCheck = () => {
+    this.setState({ check: true });
+  };
   handleUser() {
     const booking = JSON.parse(localStorage.getItem("userBooking"));
     this.setState({ booking });
+    this.setCheck();
   }
   render() {
-    // const localsBooked = this.state.booking.map((local, i) => (
-    //   <Accordion
-    //     bio={local.bio}
-    //     name={`${local.firstname} ${local.lastname}`}
-    //     rate={local.rate}
-    //     key={i}
-    //     rating={local.rating}
-    //     tourInfo={local.tourInfo}
-    //     startDate={
-    //       <Moment format="MM/DD/YYYY">{local.availability[0].dateStart}</Moment>
-    //     }
-    //     endDate={
-    //       <Moment format="MM/DD/YYYY">{local.availability[0].dateEnd}</Moment>
-    //     }
-    //   />
-    // ));
+    let bookingsArray = [];
+    for (let i = 0; i < this.state.booking.length; i++) {
+      API.getLocalById(this.state.booking[i].id).then(result =>
+        bookingsArray.push(result)
+      );
+    }
+    console.log(bookingsArray);
+
+    bookingsArray.filter((item, index) => {
+      return bookingsArray.indexOf(item) === index;
+    });
+
+    console.log(bookingsArray);
+    let localsBooked;
+    if (this.state.check) {
+      localsBooked = this.state.booking.map((local, i) => (
+        <Accordion
+          bio={local.bio}
+          name={`${local.firstname} ${local.lastname}`}
+          rate={local.rate}
+          key={i}
+          rating={local.rating}
+          tourInfo={local.tourInfo}
+          startDate={
+            <Moment format="MM/DD/YYYY">
+              {local.availability[0].dateStart}
+            </Moment>
+          }
+          endDate={
+            <Moment format="MM/DD/YYYY">{local.availability[0].dateEnd}</Moment>
+          }
+        />
+      ));
+    }
     console.log(this.state);
 
     return (
@@ -42,7 +67,7 @@ class ToursBody extends React.Component {
         </div>
         <div className="traveler-upcoming-tours">Upcoming Tours</div>
         <div className="traveler-upcoming-tours-accordions">
-          {/* {localsBooked} */}
+          {localsBooked}
           <Accordion content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." />
           <Accordion content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." />
         </div>
