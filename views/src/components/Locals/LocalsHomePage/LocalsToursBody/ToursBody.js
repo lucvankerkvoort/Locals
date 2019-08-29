9f92639923d31703f52dd64da5fdbb67bc630323
Tangerinez <gtang159@gmail.com>
2019-08-29 16:20:49 -0700

    Finished logic for tours section on both travelers and locals page. Updated UI across the whole application - still need to touch up locals calendar section

--- views/src/components/Locals/LocalsHomePage/LocalsToursBody/ToursBody.js
+++ views/src/components/Locals/LocalsHomePage/LocalsToursBody/ToursBody.js
@@ -6,13 +6,31 @@ class ToursBody extends React.Component {
   state = {
     tours: JSON.parse(localStorage.getItem("localBooking"))
   };
+  nextBooking;
   bookingInfo;
   render() {
-    if (this.state.tours) {
+    const nextTour = this.state.tours[0];
+    if (nextTour) {
+      this.nextBooking = (
+        <Accordion
+          name={`${nextTour.firstname} ${nextTour.lastname}`}
+          key={0}
+          userId={nextTour._id}
+        />
+      );
+    } else {
+      this.nextBooking = (
+        <p className="warning-message-local-tours">No tours yet!</p>
+      );
+    }
+
+    const allBookings = this.state.tours;
+    allBookings.shift();
+    if (allBookings.length > 0) {
       this.bookingInfo = this.state.tours.map((traveler, i) => (
         <Accordion
           name={`${traveler.firstname} ${traveler.lastname}`}
-          key={i}
+          key={i + 1}
           userId={traveler._id}
         />
       ));
@@ -26,13 +44,10 @@ class ToursBody extends React.Component {
         </p>
       );
     }
-
     return (
       <div className="local-body-container">
         <div className="local-next-tour">Next Tour</div>
-        <div className="local-next-tour-accordion">
-          <Accordion content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." />
-        </div>
+        <div className="local-next-tour-accordion">{this.nextBooking}</div>
         <div className="local-upcoming-tours">Upcoming Tours</div>
         <div className="local-upcoming-tours-accordions">
           {this.bookingInfo}
