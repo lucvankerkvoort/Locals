5c4319a02d56dd4269f31b5396d3704b2fe71f88
Chiefcos <41163851+Chiefcos@users.noreply.github.com>
2019-08-29 12:38:20 -0700

    fixed the tours page on the traveler side when you have no bookings

--- views/src/components/Travelers/TravelerToursPage/ToursBody/ToursBody.js
+++ views/src/components/Travelers/TravelerToursPage/ToursBody/ToursBody.js
@@ -8,29 +8,40 @@ class ToursBody extends React.Component {
     booking: JSON.parse(localStorage.getItem("userBooking")),
     check: false
   };
+  localsBooked;
 
   render() {
     console.log(this.state.booking);
-    const localsBooked = this.state.booking.map((local, i) => {
-      return (
-        <Accordion
-          content={local.bio}
-          name={`${local.firstname} ${local.lastname}`}
-          rate={local.rate}
-          key={i}
-          rating={local.rating}
-          tourInfo={local.tourInfo}
-          startDate={
-            <Moment format="MM/DD/YYYY">
-              {local.availability[0].dateStart}
-            </Moment>
-          }
-          endDate={
-            <Moment format="MM/DD/YYYY">{local.availability[0].dateEnd}</Moment>
-          }
-        />
+    if (this.state.booking) {
+      this.localsBooked = this.state.booking.map((local, i) => {
+        return (
+          <Accordion
+            content={local.bio}
+            name={`${local.firstname} ${local.lastname}`}
+            rate={local.rate}
+            key={i}
+            rating={local.rating}
+            tourInfo={local.tourInfo}
+            startDate={
+              <Moment format="MM/DD/YYYY">
+                {local.availability[0].dateStart}
+              </Moment>
+            }
+            endDate={
+              <Moment format="MM/DD/YYYY">
+                {local.availability[0].dateEnd}
+              </Moment>
+            }
+          />
+        );
+      });
+    } else {
+      this.localsBooked = (
+        <p className="warning-message-traveler-tour">
+          Nothing to show, make a booking
+        </p>
       );
-    });
+    }
 
     console.log(this.state.bookedLocals);
 
@@ -44,7 +55,7 @@ class ToursBody extends React.Component {
         </div>
         <div className="traveler-upcoming-tours">Upcoming Tours</div>
         <div className="traveler-upcoming-tours-accordions">
-          {localsBooked}
+          {this.localsBooked}
           {/* <Accordion content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." />
           <Accordion content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." /> */}
         </div>
